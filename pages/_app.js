76e128c16478f8156fa0312bf4decfebc40edfef
Matt Van Veenendaal <matt@opticpower.com>
2020-08-25 17:04:58 -0400

    adding light and dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
-import { GeistProvider, CssBaseline } from '@geist-ui/react';
+import { useState } from 'react';
+import { GeistProvider, CssBaseline, Text, Row, Col, Spacer, ButtonDropdown } from '@geist-ui/react';
+import { CloudLightning, Sun, Moon } from '@geist-ui/react-icons';
 
 const MyApp = ({ Component, pageProps }) => {
+  const [useLight, setUseLight] = useState(false);
+
   return (
-    <GeistProvider theme={{ type: 'dark' }}>
+    <GeistProvider theme={{ type: useLight ? 'light' : 'dark' }}>
       <CssBaseline />
+      <Row>
+        <Col>
+          <Text h5>Optic Tracker</Text>
+        </Col>
+        <Col align="right">
+          <ButtonDropdown size="mini" auto>
+            <ButtonDropdown.Item main={useLight === false} onClick={() => setUseLight(false)}>
+              <Moon size={16} /> <Spacer inline x={0.35} />
+              Dark Mode
+            </ButtonDropdown.Item>
+            <ButtonDropdown.Item main={Boolean(useLight)} onClick={() => setUseLight(true)}>
+              <Sun size={16} /> <Spacer inline x={0.35} />
+              Light Mode
+            </ButtonDropdown.Item>
+          </ButtonDropdown>
+        </Col>
+      </Row>
       <Component {...pageProps} toggleLight />
     </GeistProvider>
   );
