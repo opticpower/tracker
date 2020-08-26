061430dbe81e27bec2a3ecaf7c23827c7bf9aa1f
Matt Van <matt@opticpower.com>
2020-08-26 13:25:06 -0400

    Update components/ProjectPicker.tsx

--- components/ProjectPicker.tsx
+++ components/ProjectPicker.tsx
@@ -14,7 +14,6 @@ const ProjectPicker = ({ id }): JSX.Element => {
   const router = useRouter();
   const dispatch = useDispatch();
 
-  console.log('have project', projects);
 
   useEffect(() => {
     if (!projects.length) {
