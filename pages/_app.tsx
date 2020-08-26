0487e13a0bee93bf3fd3820813af74814ee7de24
Matt Van Veenendaal <matt@opticpower.com>
2020-08-26 12:50:38 -0400

    Adding redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
+import React, { useState, FC } from 'react';
 import { GeistProvider, CssBaseline, Text, Row, Col, Spacer, ButtonDropdown } from '@geist-ui/react';
-import { CloudLightning, Sun, Moon } from '@geist-ui/react-icons';
+import { Sun, Moon } from '@geist-ui/react-icons';
+import { wrapper } from '../redux/store';
+import { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }) => {
-  const [useLight, setUseLight] = useState(false);
+const OpticTracker: FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
+  const [useLight, setUseLight] = useState<boolean>(false);
 
   return (
     <GeistProvider theme={{ type: useLight ? 'light' : 'dark' }}>
       <CssBaseline />
       <Row>
         <Col>
+          <Spacer x={1} />
           <Text h5>Optic Tracker</Text>
         </Col>
         <Col align="right">
           <ButtonDropdown size="mini" auto>
-            <ButtonDropdown.Item main={useLight === false} onClick={() => setUseLight(false)}>
+            <ButtonDropdown.Item main={useLight === false} onClick={(): void => setUseLight(false)}>
               <Moon size={16} /> <Spacer inline x={0.35} />
               Dark Mode
             </ButtonDropdown.Item>
-            <ButtonDropdown.Item main={Boolean(useLight)} onClick={() => setUseLight(true)}>
+            <ButtonDropdown.Item main={Boolean(useLight)} onClick={(): void => setUseLight(true)}>
               <Sun size={16} /> <Spacer inline x={0.35} />
               Light Mode
             </ButtonDropdown.Item>
@@ -29,4 +32,4 @@ const MyApp = ({ Component, pageProps }) => {
     </GeistProvider>
   );
 };
-export default MyApp;
+export default wrapper.withRedux(OpticTracker);
---
+++ redux/reducers/projects.ts
@@ -0,0 +1,17 @@
+import { Project } from '../types';
+import { AnyAction } from 'redux';
+import { ADD_PROJECTS } from '../actions/projects.actions';
+
+const initialState = [];
+
+// create your reducer
+const reducer = (state: Project[] = initialState, action: AnyAction) => {
+  switch (action.type) {
+    case ADD_PROJECTS:
+      return [...action.projects];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
