5d02b862500afb165c85a16b8eeb3adf9d23cdf9
Roberto <roberto@opticpower.com>
2020-08-31 11:04:08 -0400

    Clean up

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,9 @@
-import { Text, Row, Col, Spacer, Select } from '@geist-ui/react';
+import { Row, Col, Spacer, Select } from '@geist-ui/react';
 import { Sun, Moon } from '@geist-ui/react-icons';
 
 const opticLogo = '/images/OpticPower.png';
 
-const Nav = ({ useLight, setUseLight }) => {
+const Nav = ({ useLight, setUseLight }): JSX.Element => {
   return (
     <Row className="nav">
       <style jxs>
@@ -26,11 +26,11 @@ const Nav = ({ useLight, setUseLight }) => {
       </Col>
       <Col align="right">
         <Select placeholder={useLight ? 'Light Mode' : 'Dark Mode'}>
-          <Select.Option value="Dark Mode" onClick={() => setUseLight(false)}>
+          <Select.Option value="Dark Mode" onClick={(): void => setUseLight(false)}>
             <Moon size={16} /> <Spacer inline x={0.35} />
             Dark Mode
           </Select.Option>
-          <Select.Option value="Light Mode" onClick={() => setUseLight(true)}>
+          <Select.Option value="Light Mode" onClick={(): void => setUseLight(true)}>
             <Sun size={16} /> <Spacer inline x={0.35} />
             Light Mode
           </Select.Option>
