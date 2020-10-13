33b9b4d19f35494e3695877dfa20cfa7f9f5c769
Mike Alche <mikealche@gmail.com>
2020-10-13 13:18:09 -0300

    Move to promise all

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,6 +1,8 @@
 import { Col, Loading, Row } from '@geist-ui/react';
 import { useTheme } from '@geist-ui/react';
+import axios, { AxiosResponse } from 'axios';
 import { subDays } from 'date-fns';
+import { access } from 'fs';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -24,216 +26,229 @@ import { Filters, Iteration, Label, Owner, State, Story } from '../../redux/type
 import { spacing } from '../../styles';
 
 const states = [
-    'unscheduled',
-    'unstarted',
-    'started',
-    'finished',
-    'delivered',
-    'rejected',
-    'accepted'
+  'unscheduled',
+  'unstarted',
+  'started',
+  'finished',
+  'delivered',
+  'rejected',
+  'accepted'
 ];
 
 interface Params {
-    id?: string;
+  id?: string;
 }
 
 const Container = styled.div(({ color, image }) => ({
-    overflow: 'auto',
-    overflowX: 'auto',
-    backgroundColor: color,
-    backgroundImage: `url(/images/grid-${image}.png)`,
-    height: '100%',
-    minHeight: 1024,
-    paddingTop: spacing(3)
+  overflow: 'auto',
+  overflowX: 'auto',
+  backgroundColor: color,
+  backgroundImage: `url(/images/grid-${image}.png)`,
+  height: '100%',
+  minHeight: 1024,
+  paddingTop: spacing(3)
 }));
 
 const Project: NextPage = (): JSX.Element => {
-    // TODO: move filter container to a separate component
-    const FilterContainer = styled.div`
-        padding: 10px 16px;
-        & > * {
-            vertical-align: middle;
-            margin: 0 4px;
-        }
-    `;
+  // TODO: move filter container to a separate component
+  const FilterContainer = styled.div`
+    padding: 10px 16px;
+    & > * {
+      vertical-align: middle;
+      margin: 0 4px;
+    }
+  `;
 
-    const router = useRouter();
-    const { id }: Params = router.query;
-    const dispatch = useDispatch();
-    const [filters, setFilters] = useState<Filters>({});
-    const [selectedStory, setSelectedStory] = useState<Story>();
-    const apiKey = useSelector(getApiKey);
-    const stories = useSelector(
-        (state: State): Record<string, Story[]> => filterStories(state, id, filters)
-    );
+  const router = useRouter();
+  const { id }: Params = router.query;
+  const dispatch = useDispatch();
+  const [filters, setFilters] = useState<Filters>({});
+  const [selectedStory, setSelectedStory] = useState<Story>();
+  const apiKey = useSelector(getApiKey);
+  const stories = useSelector(
+    (state: State): Record<string, Story[]> => filterStories(state, id, filters)
+  );
 
-    const getFilterArray = (filters: Owner[] | Label[] = [], filter: Owner | Label) => {
-        if (filters.find((f) => f.name === filter.name)) {
-            return filters;
-        }
-        return [...filters, filter];
-    };
+  const getFilterArray = (filters: Owner[] | Label[] = [], filter: Owner | Label) => {
+    if (filters.find((f) => f.name === filter.name)) {
+      return filters;
+    }
+    return [...filters, filter];
+  };
 
-    const addFilter = (name: string, filter: Owner | Label | Iteration): void => {
-        if (name === 'iterations') {
-            //todo: we should change this check to a type check and uses classes so we don't have to do this.
-            // @ts-ignore: we know iteration can only be Iteration type
-            setFilters({ ...filters, iteration: filter });
-            return;
-        }
-        // @ts-ignore: we know filter cannot be type Iteration
-        setFilters({ ...filters, [name]: getFilterArray(filters[name], filter) });
-    };
+  const addFilter = (name: string, filter: Owner | Label | Iteration): void => {
+    if (name === 'iterations') {
+      //todo: we should change this check to a type check and uses classes so we don't have to do this.
+      // @ts-ignore: we know iteration can only be Iteration type
+      setFilters({ ...filters, iteration: filter });
+      return;
+    }
+    // @ts-ignore: we know filter cannot be type Iteration
+    setFilters({ ...filters, [name]: getFilterArray(filters[name], filter) });
+  };
 
-    const removeFilter = (name: string, filter: Owner | Label | Iteration): void => {
-        if (name === 'iterations') {
-            const { iteration: omit, ...newFilters } = filters;
-            setFilters({ ...newFilters });
-            return;
-        }
-        setFilters({
-            ...filters,
-            [name]: [...filters[name].filter((element: any): boolean => element !== filter)]
-        });
-    };
-
-    const openStory = (story: Story) => {
-        setSelectedStory(story);
-    };
-
-    const closeStory = () => {
-        //todo: should we save selected story on close or nawh?
-        setSelectedStory(undefined);
-    };
-
-    useEffect(() => {
-        if (!id) {
-            return;
-        }
-        const getStories = async () => {
-            let stories = {};
-
-            //todo: we should do Promise.all for these
-            for (const state of states) {
-                let fetchString = `stories?limit=500&with_state=${state}&fields=name,estimate,owners,labels,blockers,reviews,story_type,description`;
-                if (state === 'Accepted') {
-                    const oneWeekAgo = subDays(new Date(), 7);
-                    fetchString = `${fetchString}&accepted_after=${oneWeekAgo.getTime()}`;
-                }
-
-                const request = await fetch(
-                    `https://www.pivotaltracker.com/services/v5/projects/${id}/${fetchString}`,
-                    {
-                        headers: {
-                            'X-TrackerToken': apiKey
-                        }
-                    }
-                );
-                stories = { ...stories, [state]: await request.json() };
-            }
-
-            dispatch(addStories({ id, stories }));
-        };
-        getStories();
-    }, [id]);
-
-    const [, onDragEnd] = useAsync(async (result: DragDropContext.result) => {
-        const {
-            source: { droppableId: sourceDroppableId, index: sourceIndex },
-            destination,
-            destination: { droppableId: destinationDroppableId, index: destinationIndex },
-            draggableId
-        } = result;
-
-        if (!destination) {
-            return;
-        }
-
-        if (destinationDroppableId === sourceDroppableId && destinationIndex === sourceIndex) {
-            return;
-        }
-
-        const sourceState = states[sourceDroppableId];
-        const destinationState = states[destinationDroppableId];
-
-        dispatch(
-            moveStory({
-                projectId: id,
-                sourceState,
-                sourceIndex,
-                destinationState,
-                destinationIndex
-            })
-        );
-
-        const landingIndex =
-            // Calculates the index in between which two stories the dragged story landed.
-            destinationDroppableId === sourceDroppableId && destinationIndex > sourceIndex
-                ? // Special case when landing further down from the same column the story was taken.
-                  destinationIndex + 1
-                : destinationIndex;
-
-        const payload = {
-            current_state: destinationState,
-            before_id: stories[destinationState][landingIndex]?.id || null,
-            // A null before_id means the story was placed first in the list.
-            after_id: stories[destinationState][landingIndex - 1]?.id || null
-            // A null after_id means the story was placed last in the list.
-        };
-        await fetch(
-            `https://www.pivotaltracker.com/services/v5/projects/${id}/stories/${draggableId}`,
-            {
-                method: 'PUT',
-                headers: {
-                    'X-TrackerToken': apiKey,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ ...payload })
-            }
-        );
+  const removeFilter = (name: string, filter: Owner | Label | Iteration): void => {
+    if (name === 'iterations') {
+      const { iteration: omit, ...newFilters } = filters;
+      setFilters({ ...newFilters });
+      return;
+    }
+    setFilters({
+      ...filters,
+      [name]: [...filters[name].filter((element: any): boolean => element !== filter)]
     });
+  };
 
-    const loading = !(stories && Object.values(stories).length);
-    const { palette, type } = useTheme();
+  const openStory = (story: Story) => {
+    setSelectedStory(story);
+  };
 
-    return (
-        <Container color={palette.accents_1} image={type}>
-            <StoryModal isOpen={Boolean(selectedStory)} story={selectedStory} close={closeStory} />
-            <FilterContainer>
-                <ProjectPicker id={id} />
-                <IterationPicker
-                    id={id}
-                    selectedIteration={filters.iteration}
-                    addIteration={(val) => addFilter('iterations', val)}
-                    removeIteration={() => removeFilter('iterations', null)}
-                />
-                <Labels labels={filters.labels} onClick={removeFilter} />
-                <Owners owners={filters.owners} onClick={removeFilter} />
-            </FilterContainer>
+  const closeStory = () => {
+    //todo: should we save selected story on close or nawh?
+    setSelectedStory(undefined);
+  };
 
-            <Row gap={0.8}>
-                {loading && (
-                    <Col>
-                        <Loading />
-                    </Col>
-                )}
-                {!loading && (
-                    <DragDropContext onDragEnd={onDragEnd}>
-                        {states.map((state: string, idx: number) => (
-                            <Column
-                                key={state}
-                                state={state}
-                                idx={idx}
-                                stories={stories[state]}
-                                addFilter={addFilter}
-                                openStory={openStory}
-                            />
-                        ))}
-                    </DragDropContext>
-                )}
-            </Row>
-        </Container>
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    const getStories = async () => {
+      const getFetchStringFromState = (state: string) => {
+        let fetchString = `stories?limit=500&with_state=${state}&fields=name,estimate,owners,labels,blockers,reviews,story_type,description`;
+        if (state === 'Accepted') {
+          const oneWeekAgo = subDays(new Date(), 7);
+          fetchString = `${fetchString}&accepted_after=${oneWeekAgo.getTime()}`;
+        }
+        return fetchString;
+      };
+
+      const getStoriesForFetchString = (apiKey: string, projectId: string) => (
+        fetchString: string
+      ) =>
+        axios.get(
+          `https://www.pivotaltracker.com/services/v5/projects/${projectId}/${fetchString}`,
+          {
+            headers: {
+              'X-TrackerToken': apiKey
+            }
+          }
+        );
+
+      const formatResult = (acc: Record<string, Story[]>, stories: Story[], idx: number) => ({
+        ...acc,
+        [states[idx]]: stories
+      });
+
+      const stories = (
+        await Promise.all(
+          states.map(getFetchStringFromState).map(getStoriesForFetchString(apiKey, id))
+        )
+      )
+        .map(({ data }: AxiosResponse): Story[] => data)
+        .reduce(formatResult, {} as Record<string, Story[]>);
+
+      dispatch(addStories({ id, stories }));
+    };
+    getStories();
+  }, [id]);
+
+  const [, onDragEnd] = useAsync(async (result: DragDropContext.result) => {
+    const {
+      source: { droppableId: sourceDroppableId, index: sourceIndex },
+      destination,
+      destination: { droppableId: destinationDroppableId, index: destinationIndex },
+      draggableId
+    } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destinationDroppableId === sourceDroppableId && destinationIndex === sourceIndex) {
+      return;
+    }
+
+    const sourceState = states[sourceDroppableId];
+    const destinationState = states[destinationDroppableId];
+
+    dispatch(
+      moveStory({
+        projectId: id,
+        sourceState,
+        sourceIndex,
+        destinationState,
+        destinationIndex
+      })
     );
+
+    const landingIndex =
+      // Calculates the index in between which two stories the dragged story landed.
+      destinationDroppableId === sourceDroppableId && destinationIndex > sourceIndex
+        ? // Special case when landing further down from the same column the story was taken.
+          destinationIndex + 1
+        : destinationIndex;
+
+    const payload = {
+      current_state: destinationState,
+      before_id: stories[destinationState][landingIndex]?.id || null,
+      // A null before_id means the story was placed first in the list.
+      after_id: stories[destinationState][landingIndex - 1]?.id || null
+      // A null after_id means the story was placed last in the list.
+    };
+    await fetch(
+      `https://www.pivotaltracker.com/services/v5/projects/${id}/stories/${draggableId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'X-TrackerToken': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ...payload })
+      }
+    );
+  });
+
+  const loading = !(stories && Object.values(stories).length);
+  const { palette, type } = useTheme();
+
+  return (
+    <Container color={palette.accents_1} image={type}>
+      <StoryModal isOpen={Boolean(selectedStory)} story={selectedStory} close={closeStory} />
+      <FilterContainer>
+        <ProjectPicker id={id} />
+        <IterationPicker
+          id={id}
+          selectedIteration={filters.iteration}
+          addIteration={(val) => addFilter('iterations', val)}
+          removeIteration={() => removeFilter('iterations', null)}
+        />
+        <Labels labels={filters.labels} onClick={removeFilter} />
+        <Owners owners={filters.owners} onClick={removeFilter} />
+      </FilterContainer>
+
+      <Row gap={0.8}>
+        {loading && (
+          <Col>
+            <Loading />
+          </Col>
+        )}
+        {!loading && (
+          <DragDropContext onDragEnd={onDragEnd}>
+            {states.map((state: string, idx: number) => (
+              <Column
+                key={state}
+                state={state}
+                idx={idx}
+                stories={stories[state]}
+                addFilter={addFilter}
+                openStory={openStory}
+              />
+            ))}
+          </DragDropContext>
+        )}
+      </Row>
+    </Container>
+  );
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(redirectIfNoApiKey);
