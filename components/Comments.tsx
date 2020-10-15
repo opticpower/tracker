254f27b2bdf4f33f0b8cbf0eeb95e18e9c7f8275
Scott <scott6306@gmail.com>
2020-10-15 10:37:44 -0700

    style story modal

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,7 +1,8 @@
-import { Divider, Spacer, Text } from '@geist-ui/react';
+import { Card, Divider, Text } from '@geist-ui/react';
 import { formatRelative, parseISO } from 'date-fns';
 import { Fragment } from 'react';
 import Markdown from 'react-markdown';
+import styled from 'styled-components';
 
 import { Comment, Story } from '../redux/types';
 import AddComment from './AddComment';
@@ -10,6 +11,22 @@ import Person from './Person';
 interface CommentsParams {
   story: Story;
 }
+
+const AuthorHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const DateText = styled(Text).attrs({ small: true })`
+  color: ${props => `${props.theme.palette.accents_4} !important`};
+`;
+
+const CommentContainer = styled.div`
+  padding: 5px 0px 30px 50px !important;
+  & .card {
+    border-color: ${props => `${props.theme.palette.accents_2} !important`};
+  }
+`;
 
 const Comments = ({ story }: CommentsParams): JSX.Element => {
   const comments = story.comments;
@@ -23,17 +40,18 @@ const Comments = ({ story }: CommentsParams): JSX.Element => {
   }
   return (
     <>
-      <Divider>Comments</Divider>
       {comments.map(
         (comment: Comment): JSX.Element => (
           <Fragment key={comment.id}>
-            {}
-            <Person person={comment.person} />
-            <Spacer y={1} />
-            <Text small>{formatRelative(parseISO(comment.created_at), new Date())}</Text>
-            <Spacer y={1} />
-            <Markdown>{comment.text}</Markdown>
-            <Divider />
+            <AuthorHeader>
+              <Person person={comment.person} />
+              <DateText>{formatRelative(parseISO(comment.created_at), new Date())}</DateText>
+            </AuthorHeader>
+            <CommentContainer>
+              <Card>
+                <Markdown>{comment.text}</Markdown>
+              </Card>
+            </CommentContainer>
           </Fragment>
         )
       )}
--- components/StoryModal.tsx
+++ components/StoryModal.tsx
@@ -1,6 +1,7 @@
-import { Modal } from '@geist-ui/react';
+import { Divider, Modal, Text } from '@geist-ui/react';
 import Markdown from 'react-markdown';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 import { deselectStory } from '../redux/actions/selectedStory.actions';
 import { getSelectedStory, isStorySelected } from '../redux/selectors/selectedStory.selectors';
@@ -8,6 +9,21 @@ import Blockers from './Blockers';
 import Comments from './Comments';
 import Labels from './Labels';
 import Owners from './Owners';
+
+const SectionContainer = styled.div`
+  &:not(last-child) {
+    margin-bottom: 24px;
+  }
+`;
+
+const Section = ({ title, children }): JSX.Element => (
+  <SectionContainer>
+    <Text h6 type="secondary">
+      {title}
+    </Text>
+    {children}
+  </SectionContainer>
+);
 
 const StoryModal = (): JSX.Element => {
   const dispatch = useDispatch();
@@ -18,10 +34,17 @@ const StoryModal = (): JSX.Element => {
     <Modal open={isOpen} key={story?.id} width="60%" onClose={() => dispatch(deselectStory())}>
       <Modal.Title>{story?.name}</Modal.Title>
       <Modal.Content>
-        <Markdown>{story?.description}</Markdown>
-        <Owners owners={story?.owners} />
-        <Labels labels={story?.labels} />
+        <Section title="Description">
+          <Markdown>{story?.description || 'Add something...'}</Markdown>
+        </Section>
+        <Section title="Owners">
+          <Owners owners={story?.owners} />
+        </Section>
+        <Section title="Tags">
+          <Labels labels={story?.labels} />
+        </Section>
         <Blockers blockers={story?.blockers} />
+        <Divider>Comments</Divider>
         <Comments story={story} />
       </Modal.Content>
     </Modal>
