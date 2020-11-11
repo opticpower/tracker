0ad1db085f4a02cac9418d06a6f30094abf7a1fc
Ramiro Piñol <r.pinol@opticpower.com>
2020-11-11 16:19:22 -0300

    feat: added reviews in card view
    * new: reviews and reviewers can be seen in card now

---
+++ components/ReviewBadge.tsx
@@ -0,0 +1,83 @@
+import { Text, Tooltip } from '@geist-ui/react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { getPeople } from '../redux/selectors/projects.selectors';
+import { Owner, State } from '../redux/types';
+
+const statusColors = {
+  unstarted: 'grey',
+  in_review: 'yellow',
+  pass: 'green',
+  revise: 'red',
+};
+
+interface OwnersLinksParams {
+  ownersData: Owner[] | null;
+  onClick?: (name: string, filter: Owner) => void;
+}
+interface ReviewBadgeParams {
+  type: string;
+  status: string;
+  ownersIds?: number[];
+  onClick?: (name: string, filter: Owner) => void;
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  height: 16px;
+  width: 16px;
+  margin-right: 4px;
+
+  border-radius: 16px;
+  background: ${props => statusColors[props.status] || 'grey'};
+`;
+
+const ReviewerText = styled(Text)`
+  cursor: pointer;
+`;
+
+const OwnersLinks = ({ ownersData, onClick }: OwnersLinksParams): JSX.Element => {
+  return (
+    <>
+      {' ('}
+      {ownersData.map(owner => (
+        <Tooltip key={`tooltip-${owner.id}`} type="secondary" text={owner.name}>
+          <ReviewerText
+            type="success"
+            size={12}
+            span
+            key={`link-${owner.id}`}
+            onClick={(): void => onClick('owners', owner)}>
+            {owner.initials}
+          </ReviewerText>
+        </Tooltip>
+      ))}
+      {')'}
+    </>
+  );
+};
+
+const ReviewBadge = ({ type, status, ownersIds, onClick }: ReviewBadgeParams): JSX.Element => {
+  const ownersData = ownersIds?.length
+    ? useSelector((state: State): Owner[] => getPeople(state, ownersIds))
+    : null;
+
+  return (
+    <Container>
+      <Tooltip type="secondary" text={status}>
+        <Icon status={status} />
+      </Tooltip>
+      <Text size={12} span>
+        {type}
+        {Boolean(ownersData.length) && <OwnersLinks ownersData={ownersData} onClick={onClick} />}
+      </Text>
+    </Container>
+  );
+};
+
+export default ReviewBadge;
--- redux/selectors/projects.selectors.ts
+++ redux/selectors/projects.selectors.ts
@@ -4,10 +4,16 @@ export const getProjectName = (state: State, id: string): string => {
   return state.projects.find(project => String(project.id) === String(id))?.name;
 };
 
-export const getPeople = (state: State): Owner[] => {
-  return state.projects.find(
+export const getPeople = (state: State, ids?: number[]): Owner[] => {
+  const people = state.projects.find(
     project => String(project.id) === String(state.stories?.selectedProjectId)
   )?.people;
+
+  if (ids?.length) {
+    return people.filter(person => ids.includes(Number(person.id)));
+  } else {
+    return people;
+  }
 };
 
 export const getReviewTypes = (state: State, id: string): ReviewTypesObj => {
